f5d9bd392bd888bb48354626342fd91f85cda25c
Matt <matt@promptmule.com>
2024-01-21 20:49:56 -0800

    initial push
    first take a multi-llm comparison chatbot. Include local and SaaS LLMs.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# multi-llm-testing
- A simple chatbot to compare answers from local and SaaS LLMs
+# Multi-Model Language Generation Interface
+
+This repository contains code for an interactive language generation interface that integrates multiple language models, including OpenAI's GPT-3.5, Anthropic's Claude-2.1, and models from Google Vertex AI. The interface is built using Panel for a web-based chat experience and allows users to interact with these models in real-time.
+
+## Features
+
+- Integration with multiple language models including OpenAI's GPT-3.5, Anthropic's Claude-2.1, and Google Vertex AI's generative models.
+- Real-time interaction through a web-based chat interface.
+- Response latency measurement for each model.
+- Response comparison across different models.
+- CSV logging of user prompts, model responses, and response latencies.
+
+## Installation
+
+Before running the script, ensure you have the required dependencies installed:
+
+```bash
+pip install panel openai google-cloud-aiplatform
+```
+
+## Setup
+
+1. **OpenAI API Key**: Replace `'sk-ABC'` with your actual OpenAI API key.
+2. **Anthropic API Key**: Replace `'sk-ant-api03-XDGXYZ'` with your actual Anthropic API key.
+3. **Google Cloud Project ID**: Set `google_project_id` to your Google Cloud project ID.
+4. **Google Cloud Location ID**: Set `google_location_id` to the appropriate Google Cloud location.
+
+## Usage
+
+Run the script to start the server:
+
+```bash
+python your_script_name.py
+```
+
+Access the web-based interface at the provided URL to interact with the language models.
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome. Feel free to check [issues page]() if you want to contribute.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+
+Matt Rodgers - mrodgers.junk@gmail.com
+
+Project Link: [https://github.com/](https://github.com/)
---
+++ multi-llm-testing.py
@@ -0,0 +1,169 @@
+import panel as pn
+from ctransformers import AutoModelForCausalLM
+import openai
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+#from google.cloud import aiplatform
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part
+import time
+import csv
+import asyncio
+import os
+
+pn.extension()
+
+# OpenAI API key (replace with your actual API key)
+openai.api_key = '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'
+
+# Anthropic API setup
+anthropic = Anthropic(api_key="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")  # Replace with your actual API key
+
+# Google Vertex API, this is the google project you are using, also you will need to be logged in to google api
+# pip3 install --upgrade --user google-cloud-aiplatform
+# gcloud auth application-default login
+google_project_id = 'test-cache-12242023'
+google_location_id = 'us-west1'
+
+# this is the file where responses are saved along side of each other, with latencies
+response_filename = 'llm_responses.csv'
+
+
+
+MODEL_ARGUMENTS = {
+    "samantha": {
+        "args": ["TheBloke/Dr_Samantha-7B-GGUF"],
+        "kwargs": {"model_file": "dr_samantha-7b.Q5_K_M.gguf"},
+    },
+    "llama": {
+        "args": ["TheBloke/Llama-2-7b-Chat-GGUF"],
+        "kwargs": {"model_file": "llama-2-7b-chat.Q5_K_M.gguf"},
+    },
+    "mistral": {
+        "args": ["TheBloke/Mistral-7B-Instruct-v0.1-GGUF"],
+        "kwargs": {"model_file": "mistral-7b-instruct-v0.1.Q4_K_M.gguf"},
+    },
+}
+
+# Google Vertex AI setup
+def generate_text_with_vertex_ai(project_id: str, location: str, query: str) -> str:
+    # Initialize Vertex AI
+    vertexai.init(project=project_id, location=location)
+    # Load the model
+    multimodal_model = GenerativeModel("gemini-pro-vision")
+    # Query the model
+    response = multimodal_model.generate_content(
+        [
+            # Add your query here
+            Part.from_text(query)
+        ]
+    )
+    return response.text
+
+# Function to write to CSV
+def write_to_csv(prompt, responses, latencies, filename):
+    file_exists = os.path.isfile(filename)
+    with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        # Write headers if the file is being created
+        if not file_exists:
+            headers = ['Prompt'] + [f'Response from LLM {i+1}' for i in range(len(responses))] + [f'Latency for LLM {i+1}' for i in range(len(latencies))]
+            writer.writerow(headers)
+
+        # Write the data
+        writer.writerow([prompt] + responses + latencies)
+
+async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
+    responses = []
+    latencies = []
+    
+    alpaca_prompt = f'''Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+    ### Instruction:
+    {contents}
+
+    ### Response:'''
+
+    llama_prompt = f'''
+    <s>[INST] <<SYS>>
+    You are a helpful, respectful and honest assistant.
+    <</SYS>>
+    {contents} [/INST]</s>'''
+
+    mistral_prompt = f'''
+    <s>[INST] {contents} [/INST] Model answer</s>'''
+    
+    # OpenAI response
+    start_time = time.time()
+
+    openai_response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",  # or another model like "gpt-4"
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": contents}
+        ],
+        max_tokens=512
+    )
+    openai_latency = time.time() - start_time
+    responses.append(openai_response.choices[0].message.content)
+    latencies.append(openai_latency)
+    instance.stream(openai_response.choices[0].message.content + '\nlatency: ' + str(openai_latency), user="OpenAI", message=None)
+    
+    # Anthropic response
+    start_time = time.time()
+    anthropic_response = anthropic.completions.create(
+        model="claude-2.1",
+        max_tokens_to_sample=512,
+        temperature=0,
+        prompt=f"{HUMAN_PROMPT} {contents}\n{AI_PROMPT}"
+    )
+    anthropic_latency = time.time() - start_time
+    responses.append(anthropic_response.completion)
+    latencies.append(anthropic_latency)
+    instance.stream(anthropic_response.completion + '\nlatency: ' + str(anthropic_latency), user="Anthropic", message=None)
+
+    # Google Vertex response
+    start_time = time.time()
+    vertex_ai_response = generate_text_with_vertex_ai(google_project_id, google_location_id, contents)
+    vertex_ai_latency = time.time() - start_time
+    responses.append(vertex_ai_response)
+    latencies.append(vertex_ai_latency)
+    instance.stream(vertex_ai_response+ '\nlatency: ' + str(vertex_ai_latency), user="Google Vertex AI", message=None)
+
+    
+    for model in MODEL_ARGUMENTS:
+        if model not in pn.state.cache:
+            pn.state.cache[model] = AutoModelForCausalLM.from_pretrained(
+                *MODEL_ARGUMENTS[model]["args"],
+                **MODEL_ARGUMENTS[model]["kwargs"],
+                gpu_layers=30,
+            )
+
+        llm = pn.state.cache[model]
+        
+        if model == 'samantha': prompt = alpaca_prompt
+        if model == 'llama': prompt = llama_prompt
+        if model == 'mistral': prompt = contents
+        
+        start_time = time.time()
+        response = llm(prompt, max_new_tokens=512, stream=False)
+        model_latency = time.time() - start_time
+        
+        message = None
+        #for chunk in response:
+        #    message = instance.stream(chunk, user=model.title(), message=message)
+        instance.stream(response.strip() + '\nlatency: ' + str(model_latency), user=model.title(), message=message)
+        
+        responses.append(response.strip())
+        latencies.append(model_latency)
+        
+    # Write to CSV
+    write_to_csv(contents, responses, latencies, response_filename)
+
+
+chat_interface = pn.chat.ChatInterface(callback=callback)
+chat_interface.send(
+    "Send a message to get a response from each: Llama 2, Mistral (7B), Google, Anthropic and OpenAI!",
+    user="System",
+    respond=False,
+)
+chat_interface.servable()
